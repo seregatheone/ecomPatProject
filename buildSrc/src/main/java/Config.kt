0bd302a797efd7305a81a8a630b0d1c7c053f545
seregatheone <sergeydemidovichwork@gmail.com>
2022-11-08 01:07:34 +0300

    create buildSrc folder

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,8 @@
+object Config {
+    const val nameSpace = "pat.project.ecompatproject"
+    const val compileSdkVersion = 33
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 33
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,10 @@
+object Plugins {
+    const val application = "com.android.application"
+    const val applicationVersion = "7.3.1"
+
+    const val library = "com.android.library"
+    const val libraryVersion = "7.3.1"
+
+    const val kotlin = "org.jetbrains.kotlin.android"
+    const val kotlinVersion = "1.7.20"
+}

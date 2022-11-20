a7b832bf33d77d435e1f32c0dddcc5d28dcd5a17
seregatheone <sergeydemidovichwork@gmail.com>
2022-11-20 07:45:19 +0300

    created home fragment and setup it

--- feature/home/src/main/java/pat/project/home/presentation/ui/HomeFragment.kt
+++ feature/home/src/main/java/pat/project/home/presentation/ui/HomeFragment.kt
@@ -3,22 +3,34 @@ package pat.project.home.presentation.ui
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.Lazy
 import pat.project.dagger.findDependencies
 import pat.project.ecompatproject.feature.home.R
 import pat.project.ecompatproject.feature.home.databinding.FragmentHomeBinding
 import pat.project.home.presentation.di.DaggerHomeComponent
+import pat.project.home.presentation.ui.adapters.BestSellerAdapter
+import pat.project.home.presentation.ui.adapters.HotSalesAdapter
+import pat.project.home.presentation.ui.adapters.category.CategoryAdapter
 import pat.project.network_utils.Status
 import javax.inject.Inject
 
 class HomeFragment : Fragment(R.layout.fragment_home){
 
     private val viewBinding  by viewBinding(FragmentHomeBinding::bind)
+
+    private var adapterCategory : CategoryAdapter? = null
+    private var adapterHotSales : HotSalesAdapter? = null
+    private var adapterBestSeller : BestSellerAdapter? = null
 
     @Inject
     lateinit var viewModelFactory : Lazy<HomeFragmentViewModel.Companion.HomeFragmentViewModelFactory>
@@ -33,12 +45,38 @@ class HomeFragment : Fragment(R.layout.fragment_home){
         super.onAttach(context)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        adapterCategory = CategoryAdapter(requireContext(),viewModel.currentCategory.value)
+        adapterHotSales = HotSalesAdapter(requireContext())
+        adapterBestSeller = BestSellerAdapter(requireContext())
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        with(viewBinding.selectCategoryLayout.recyclerView){
+            adapter = adapterCategory
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        }
+        with(viewBinding.bestSellerLayout.bestSellerRecyclerView){
+            adapter = adapterBestSeller
+            layoutManager = GridLayoutManager(requireContext(),2)
+        }
+        with(viewBinding.hotSalesLayout.hotSalesRecView){
+            PagerSnapHelper().attachToRecyclerView(this)
+            adapter = adapterHotSales
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        }
+
         lifecycleScope.launchWhenStarted {
             viewModel.getHomeResponseModel().collect{ resource ->
                 when(resource.status){
                     Status.SUCCESS -> {
-                        Log.i("resourceSUCCESS",resource.data.toString())
+                        adapterBestSeller!!.setData(resource.data!!.best_seller)
+                        adapterHotSales!!.setData(resource.data!!.home_store)
                     }
                     Status.ERROR -> {
                         Log.i("resourceERROR",resource.data.toString())
@@ -50,5 +88,12 @@ class HomeFragment : Fragment(R.layout.fragment_home){
             }
         }
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        adapterCategory = null
+        adapterHotSales = null
+        adapterBestSeller = null
+        super.onDestroy()
     }
 }
---
+++ feature/home/src/main/java/pat/project/home/presentation/ui/adapters/BestSellerAdapter.kt
@@ -0,0 +1,58 @@
+package pat.project.home.presentation.ui.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Paint
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import pat.project.ecompatproject.feature.home.databinding.BestSellerItemBinding
+import pat.project.home_api_interface.models.BestSeller
+
+class BestSellerAdapter(private val parentContext: Context) : RecyclerView.Adapter<BestSellerAdapter.BestSellerViewHolder>() {
+
+    private var allData: List<BestSeller> = emptyList()
+
+    inner class BestSellerViewHolder(
+        private val binding: BestSellerItemBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: BestSeller) {
+            binding.priceWith.text = item.discount_price.toString()
+            binding.priceWithout.paintFlags = binding.priceWithout.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            binding.priceWithout.text = item.price_without_discount.toString()
+            Log.i("is_favorites",item.is_favorites.toString())
+            when (item.is_favorites) {
+                true -> binding.favoriteIconPictureId.visibility = View.VISIBLE
+                false -> binding.favoriteIconPictureId.visibility = View.GONE
+            }
+            binding.name.text = item.title
+
+            Glide.with(parentContext)
+                .load(item.picture)
+                .into(binding.bestSellerPicture)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BestSellerViewHolder {
+        val itemBinding =
+            BestSellerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BestSellerViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: BestSellerViewHolder, position: Int) {
+        val currentItem = allData[position]
+        holder.bind(currentItem)
+    }
+
+    override fun getItemCount(): Int = allData.size
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(listOfBestSellers : List<BestSeller>){
+        allData = listOfBestSellers
+        notifyDataSetChanged()
+    }
+}
